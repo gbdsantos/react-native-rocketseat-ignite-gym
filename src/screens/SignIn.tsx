0b9d43ec0ce73f076cc9a59405dfa6b5d8a6125b
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-21 22:17:10 -0300

    🎨 styles(mobile): heading component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from 'native-base';
+import { Center, Heading, Image, Text, VStack } from 'native-base';
 
 import BackgroundImg from '@assets/background.png';
 import LogoSVG from '@assets/logo.svg';
@@ -19,6 +19,17 @@ export function SignIn() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading
+          color="gray.100"
+          fontFamily="heading"
+          fontSize="xl"
+          mb={6}
+        >
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   )
