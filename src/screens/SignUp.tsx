1212d259a649f219b7fa7be632ad79a671b48c24
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-24 19:24:09 -0300

    🎨 styles(mobile): modify navigate theme

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -19,7 +19,7 @@ export function SignUp() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           alt="Pessoas treinando em uma esteira"
           position="absolute"
