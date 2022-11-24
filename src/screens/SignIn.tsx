82210b6e8f360fa27dce99e76212ad6748532e5a
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-24 00:04:27 -0300

    🎨 styles(mobile): finish SignIn screen
    - use ScrollView to better user usability

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,11 @@
-import { Center, Heading, Image, Text, VStack } from 'native-base';
+import {
+  Center,
+  Heading,
+  Image,
+  ScrollView,
+  Text,
+  VStack
+} from 'native-base';
 
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
@@ -8,52 +15,60 @@ import LogoSVG from '@assets/logo.svg';
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700" px={10}>
-      <Image
-        alt="Pessoas treinando em uma esteira"
-        position="absolute"
-        resizeMode="contain"
-        source={BackgroundImg}
-      />
-
-      <Center my={24}>
-        <LogoSVG />
-
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-
-      <Center>
-        <Heading
-          color="gray.100"
-          fontFamily="heading"
-          fontSize="xl"
-          mb={6}
-        >
-          Acesse sua conta
-        </Heading>
-
-        <Input
-          autoCapitalize="none"
-          keyboardType="email-address"
-          placeholder="E-mail"
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          alt="Pessoas treinando em uma esteira"
+          position="absolute"
+          resizeMode="contain"
+          source={BackgroundImg}
         />
 
-        <Input
-          placeholder="Senha"
-          secureTextEntry
-        />
+        <Center my={24}>
+          <LogoSVG />
 
-        <Button
-          title="Acessar"
-        />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
 
-        <Button
-          title="Criar conta"
-          variant="outline"
-        />
-      </Center>
-    </VStack>
+        <Center>
+          <Heading
+            color="gray.100"
+            fontFamily="heading"
+            fontSize="xl"
+            mb={6}
+          >
+            Acesse sua conta
+          </Heading>
+
+          <Input
+            autoCapitalize="none"
+            keyboardType="email-address"
+            placeholder="E-mail"
+          />
+
+          <Input
+            placeholder="Senha"
+            secureTextEntry
+          />
+
+          <Button
+            title="Acessar"
+          />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" fontFamily="body" fontSize="sm" mb={3}>
+            Ainda não tem acesso?
+          </Text>
+
+          <Button title="Criar conta" variant="outline" />
+        </Center>
+      </VStack>
+    </ScrollView>
   )
 }
