fc91eaf959672fde4536694ddd8d9435f5110b06
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-12-08 01:09:45 -0300

    🎨 styles(mobile): customize HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,13 +2,13 @@ import { Heading, HStack, VStack, Text } from 'native-base';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack alignItems="center" bg="gray.600" pb={5} pt={16} px={8}>
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
+        <Heading color="gray.100" fontSize="md">
           Guilherme
         </Heading>
       </VStack>
