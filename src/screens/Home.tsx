baf163c1c61d75b8662d477cda1caa04c47ff260
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-12-23 16:00:24 -0300

    ✨ feat(mobile): list groups with FlatList of NativeBase

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,29 +1,33 @@
 import { useState } from 'react';
-import { HStack, VStack } from 'native-base';
+import { FlatList, HStack, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro']);
   const [groupSelected, setGroupSelected] = useState('costa');
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-        <Group
-          name="costa"
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
-
-        <Group
-          name="ombro"
-          isActive={groupSelected === "ombro"}
-          onPress={() => setGroupSelected("ombro")}
-        />
-      </HStack>
+      <FlatList
+        _contentContainerStyle={{ px: 8 }}
+        data={groups}
+        horizontal
+        keyExtractor={item => item}
+        maxH={10}
+        my={10}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+      />
     </VStack>
   );
 }
