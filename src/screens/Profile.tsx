d1224ce18c870e1e1870b5f07b26da0af4e327ea
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-01-27 19:28:25 -0300

    🎨 styles(mobile): user profile header and user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,22 @@
-import { Center, Text } from 'native-base';
+import { Center, ScrollView, VStack } from 'native-base';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            alt="Foto do usuário"
+            source={{ uri: 'https://github.com/gbdsantos.png' }}
+            size={33}
+          />
+        </Center>
+
+      </ScrollView>
+    </VStack>
   );
 }
