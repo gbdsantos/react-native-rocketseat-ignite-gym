2f34bfc3d9bf52757bab899472d7ef059c3dbddf
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-01-27 19:59:35 -0300

    ✨ feat(mobile): skeleton effect

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,19 +1,34 @@
-import { Center, ScrollView, VStack } from 'native-base';
+import { useState } from 'react'
+import { Center, Skeleton, ScrollView, VStack } from 'native-base'
 
-import { ScreenHeader } from '@components/ScreenHeader';
-import { UserPhoto } from '@components/UserPhoto';
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            alt="Foto do usuário"
-            source={{ uri: 'https://github.com/gbdsantos.png' }}
-            size={33}
-          />
+          {
+            photoIsLoading ?
+              <Skeleton
+                h={PHOTO_SIZE}
+                w={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.500"
+                endColor="gray.400"
+              /> :
+              <UserPhoto
+                alt="Foto do usuário"
+                source={{ uri: 'https://github.com/gbdsantos.png' }}
+                size={PHOTO_SIZE}
+              />
+          }
         </Center>
 
       </ScrollView>
