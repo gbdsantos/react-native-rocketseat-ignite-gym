4afa44e00fd39c7bb91e053d181640cea293d5f0
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-02-09 16:46:36 -0300

    🎨 styles(mobile): create change photo button

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { Center, Skeleton, ScrollView, VStack } from 'native-base'
+import { TouchableOpacity } from 'react-native';
+import { Center, Skeleton, ScrollView, Text, VStack } from 'native-base'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
@@ -29,6 +30,12 @@ export function Profile() {
                 size={PHOTO_SIZE}
               />
           }
+
+          <TouchableOpacity>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
         </Center>
 
       </ScrollView>
