c2d87eb9462cd2e320c69f38758ed85394c808bb
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-02-15 22:42:29 -0300

    ✨ feat(mobile): add toast message

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import { Alert, TouchableOpacity } from 'react-native';
-import { Center, Skeleton, ScrollView, Text, VStack, Heading } from 'native-base'
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  useToast,
+  VStack
+} from 'native-base'
 
 import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from 'expo-image-picker';
@@ -15,6 +23,8 @@ const PHOTO_SIZE = 33;
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
   const [userPhoto, setUserPhoto] = useState('https://github.com/gbdsantos.png')
+
+  const toast = useToast();
 
   async function handleUserPhotoSelect() {
     setPhotoIsLoading(true);
@@ -35,7 +45,11 @@ export function Profile() {
         const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
 
         if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
-          return Alert.alert("Ops...", "Essa imagem é muito grande. Escolha uma imagem de até 5MB.");
+          return toast.show({
+            bgColor: 'red.500',
+            placement: 'top',
+            title: 'Essa imagem é muito grande. Escolha uma imagem de até 5MB.',
+          });
         }
 
         setUserPhoto(photoSelected.assets[0].uri);
