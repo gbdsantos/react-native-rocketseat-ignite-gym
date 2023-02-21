5cdb1a91383441e3512d8161724fde26263d1ea9
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-02-21 16:39:05 -0300

    ✨ feat(mobile): axios exceptions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { Alert } from 'react-native';
 import {
   Center,
   Heading,
@@ -11,6 +12,7 @@ import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
+import axios from 'axios';
 import { api } from '@services/api';
 
 import { Button } from '@components/Button';
@@ -54,13 +56,18 @@ export function SignUp() {
     goBack();
   }
 
-  async function handleSignUp(data: FormDataProps) {
-    console.log(data);
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post('/users', {
+        name, email, password
+      });
 
-    const response = await api.post('/users', {
-      data
-    });
-    console.log(response.data)
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert('Ops...', error.response?.data.message);
+      }
+    }
 
     reset(defaultValues);
   }
