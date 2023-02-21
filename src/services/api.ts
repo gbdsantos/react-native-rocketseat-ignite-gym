0505453721f76a26faa5cfa03bca776e1f3a8eba
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-02-21 16:51:38 -0300

    ✍️ update(mobile): understand axios interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://192.168.1.11:3333'
 });
+
+api.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  console.log("[INTERCEPTOR RESPONSE ERROR] => ", error);
+  return Promise.reject(error);
+})
+
+export { api };
