66ee31013c8f8605cd1d9b25282e8ead77c12da9
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-03-18 21:08:36 -0300

    ✨ feat(mobile): load logged user

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,11 @@
-import { createContext, ReactNode, useState } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useState
+} from 'react';
 
-import { storageUserSave } from '@storage/storageUser';
+import { storageUserSave, storageUserGet } from '@storage/storageUser';
 
 import { UserDTO } from '@dtos/UserDTO';
 
@@ -32,6 +37,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       throw error;
     }
   }
+
+  async function loadUserData() {
+    const userLogged = await storageUserGet();
+
+    if (userLogged) {
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, [])
 
   return (
     <AuthContext.Provider value={{ user, signIn }}>
