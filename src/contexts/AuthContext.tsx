f1d1a91d816ba572f0ef993c10677a9ca57cd9f7
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-03-18 21:02:13 -0300

    ✨ feat(mobile): persist logged user

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, ReactNode, useState } from 'react';
 
+import { storageUserSave } from '@storage/storageUser';
+
 import { UserDTO } from '@dtos/UserDTO';
 
 import { api } from '@services/api';
@@ -24,6 +26,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user);
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
