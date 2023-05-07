ced0823a4dff835151e5496d769367b4e345dcfa
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-05-07 19:54:01 -0300

    ✨ feat(mobile): add requests to queue

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,14 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 
 import { AppError } from '@utils/AppError';
 import { storageAuthTokenGet } from '@storage/storageAuthToken';
 
 type SignOut = () => void;
+
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+}
 
 type APIInstanceProps= AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
@@ -12,6 +17,9 @@ type APIInstanceProps= AxiosInstance & {
 const api = axios.create({
   baseURL: 'http://192.168.1.11:3333'
 }) as APIInstanceProps;
+
+let failedQueue: Array<PromiseType> = [];
+let isRefreshing = false;
 
 api.registerInterceptTokenManager = signOut => {
   const interceptTokenManager = api.interceptors.response.use(response => response, async (requestError) => {
@@ -23,13 +31,30 @@ api.registerInterceptTokenManager = signOut => {
           signOut();
           return Promise.reject(requestError);
         }
+
+        const originalRequestConfig = requestError.config;
+
+        if(isRefreshing) {
+          return new Promise((resolve, reject) => {
+            failedQueue.push({
+              onSuccess: (token: string) => {
+                originalRequestConfig.headers = { 'Authorization': `Bearer ${token}`};
+                resolve(api(originalRequestConfig));
+              },
+              onFailure: (error: AxiosError) => {
+                reject(error);
+              },
+            })
+          });
+        }
+
+        isRefreshing = true;
       }
 
       signOut();
     }
 
     if(requestError.response && requestError.response.data) {
-      console.log(requestError.response.data);
       return Promise.reject(new AppError(requestError.response.data.message));
     } else {
       return Promise.reject(requestError);
